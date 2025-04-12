d9b89e700c31abbb645621917f7453000b129cbb
gbads <gabbiebade@gmail.com>
2025-04-11 23:36:00 -0700

    added script to close mobile menu on click of any li

--- js/child-scripts.js
+++ js/child-scripts.js
@@ -12,3 +12,34 @@ let currentScrollPos = window.scrollY;
   }
   prevScrollpos = currentScrollPos;
 }
+
+// Select all <li> elements
+const menuItems = document.querySelectorAll('li.menu-item');
+
+// Add a click event listener to each <li>
+menuItems.forEach(item => {
+ 
+  item.addEventListener('click', () => {
+    // Find the element with the 'menu-toggle' class
+    const menuToggleElement = document.getElementById("site-navigation");
+
+    const expandedBtn = document.getElementsByClassName("menu-toggle")
+    const expandedElement = document.getElementById("top-menu")
+    
+    // Remove the 'menu-toggle' class if it exists
+    if (menuToggleElement) {
+      menuToggleElement.classList.remove('toggled');
+    }
+
+    // Toggle the aria-expanded attribute on the expandedElement
+    if (expandedElement) {
+      const isExpanded = expandedElement.getAttribute('aria-expanded') === 'true';
+      expandedElement.setAttribute('aria-expanded', !isExpanded);
+    }
+    if (expandedBtn) {
+      const isExpandedBtn = expandedBtn[0].getAttribute('aria-expanded') === 'true';
+
+      expandedBtn[0].setAttribute('aria-expanded', !isExpandedBtn);
+    }
+  });
+});
